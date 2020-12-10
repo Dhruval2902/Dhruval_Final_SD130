db772204ccb6d47d698e5ef106501b33ae45c77f
Dhruvalkumar Kabirpara <dhruvkabir@student.mitt.ca>
2020-12-10 00:49:08 -0500

    concate and uglify/minify js file, corrected background image issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 const {src, dest, series} = require('gulp');
 const imagemin = require('gulp-imagemin')
+const postcss = require('gulp-postcss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
+const sourcemaps = require('gulp-sourcemaps')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify-es').default
 
 function htmlTask() {
   return src('src/*.html')
@@ -8,11 +14,17 @@ function htmlTask() {
 
 function stylesTask() {
   return src('src/*.css')
-  .pipe(dest('dist'))
+  .pipe(sourcemaps.init())
+  .pipe(postcss([ autoprefixer(), cssnano()]))
+  .pipe(sourcemaps.write())
+  .pipe(concat('all.css'))
+  .pipe(dest('dist/css'))
 }
 
 function scriptsTask() {
   return src('src/*.js')
+  .pipe(uglify())
+  .pipe(concat('all.js'))  
   .pipe(dest('dist'))
 }
 
